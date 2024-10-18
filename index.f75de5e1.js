5133623ddb91bde4b742cb86725633260a2a776d
Halyna <minimal005@gmail.com>
2024-10-18 19:09:00 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,32 @@
+"use strict";
+const table = document.querySelector("table");
+document.addEventListener("click", (e)=>{
+    const target = e.target.closest("thead th");
+    if (!target) return;
+    sort(target);
+});
+function sort(elem) {
+    // we find the column index number
+    const thead = [
+        ...table.rows[0].cells
+    ];
+    const currentPos = thead.indexOf(elem);
+    const tableBody = [
+        ...table.rows
+    ].slice(1, -1).sort((a, b)=>{
+        let firstValue = a.cells[currentPos].innerHTML;
+        let secondValue = b.cells[currentPos].innerHTML;
+        // we check whether these are numbers
+        if (parseFloat(firstValue) || firstValue.includes("$")) {
+            if (firstValue.includes("$")) {
+                firstValue = firstValue.slice(1).replace(",", ".");
+                secondValue = secondValue.slice(1).replace(",", ".");
+            }
+            return +firstValue - +secondValue;
+        }
+        return firstValue > secondValue ? 1 : -1;
+    });
+    table.tBodies[0].append(...tableBody);
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
